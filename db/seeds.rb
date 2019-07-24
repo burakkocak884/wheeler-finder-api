7faf74b2188c270bf7eb4d90dcb35bc2e8344e5f
Burak Kocak <burakkocak884@ghotmail.com>
2019-07-24 00:43:35 -0400

    database being fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,19 @@
 
 
   50.times do
-  	Vehicle.create(sale_price: Faker::Number.within(100..250)*100,
-      year: Faker::Vehicle.year,
-  		make:Faker::Vehicle.make, 
+  	Vehicle.create(
+        year: Faker::Vehicle.year,
+  		  make:Faker::Vehicle.make, 
   	    model:Faker::Vehicle.model,
   	    color: Faker::Vehicle.color,
-  	    transmission: Faker::Vehicle.transmission,
-  	    drive_type: Faker::Vehicle.drive_type,
-  	    fuel_type: Faker::Vehicle.fuel_type,
-  	    car_type: Faker::Vehicle.car_type,
-  	    style: Faker::Vehicle.style,
-  	    car_options: Faker::Vehicle.car_options,
   	    standard_specs: Faker::Vehicle.standard_specs,
-  	    license_plate: Faker::Vehicle.license_plate,
-  	    doors: Faker::Vehicle.doors,
-  	    engine: Faker::Vehicle.engine,
+  	    sale_price: Faker::Number.within(100..250)*100,
   	    mileage: Faker::Vehicle.mileage,
         
   	    
   	   )
   end 
+
  
 
   
